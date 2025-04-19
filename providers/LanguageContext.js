5d5841f1fef8fe5921363b8760b62479c77806bb
Ali Zoljalali <a.zoljalali.k@gmail.com>
2025-04-19 10:10:53 +0330

    save selected language

--- providers/LanguageContext.js
+++ providers/LanguageContext.js
@@ -1,10 +1,21 @@
 "use client";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    const storedLang = localStorage.getItem("language");
+    if (storedLang) {
+      setLanguage(storedLang);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
